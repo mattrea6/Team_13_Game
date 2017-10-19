67e0c0b12339c2c45f27838017694d6ef5effc85
mattrea6 <32769918+mattrea6@users.noreply.github.com>
2017-10-19 15:32:07 +0100

    update

--- roomsgit.py
+++ roomsgit.py
@@ -1,3 +1,29 @@
+jail_entrance = {
+	"name": "Jail Entrance",
+	"description": "You have just been released from jail.",
+	"exits": {"south":"Main Street"},
+	"items":[]
+	
+}
+
+main_street = {
+	"name": "Main Street",
+	"description": "You are on the Main Street.",
+	"exits": {"north":"Jail Entrance","south":"Grove Street","east":"Studio"},
+	"items":[]
+	
+}
+
+grove_street = {
+	"name": "Grove Street",
+	"description": "You are in your old cul-de-sac. Your old home.",
+	"exits": {"north":"Main Street"},
+	"items":[]
+	
+}
+
+
+
 room_studio = {
 	"name": "Studio",
 	"rap": "this ting go skraa, pap pap kak kak kak, skiddy skit pap pap, an a pop pop truu boom",
@@ -5,7 +31,7 @@ room_studio = {
 	"description": "The studio is small, and smells of marijuana. The coat rack is empty, man's not hot.",
 	"rapper": "Roadman Shaq",
 	"rapperbeat": False,
-	"exits": {"east": "Warehouse"},
+	"exits": {"west":"Main Street","east": "Warehouse"},
 	"items": []
 	
 }
@@ -25,5 +51,8 @@ room_warehouse = {
 
 rooms = {
     "Studio": room_studio,
-    "Warehouse": room_warehouse
+    "Warehouse": room_warehouse,
+    "Grove Street": grove_street,
+    "Main Street": main_street,
+    "Jail Entrance":jail_entrance
 }
