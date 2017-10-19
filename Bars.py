ff840f86db9b4969041e61b5c57d9bc9cbccc4bc
holycheet123 <32814987+holycheet123@users.noreply.github.com>
2017-10-19 17:07:08 +0100

    bars list(not finished)

---
+++ Bars.py
@@ -0,0 +1,54 @@
+def hot_fire_hard_bars(number):
+	1 = "Not to brag, I don't mean to boast, Look, my face is pale, but you look like you've seen a ghost."
+
+	2 = "I’m bout to make this mic short circuit. This beat is wack and fits you perfect"
+
+	3 = "I’m about eight thousand miles away from home, thinking about the throne I got my eyes fixated on" 
+
+	4 = 
+
+	5 = ""
+	6 =
+	7 =
+	8 =
+	9 =
+	10 =
+
+def mild_fire_medium_bars:
+	11 =
+	12 =
+	13 =
+	14 =
+	15 =
+	16 =
+	17 =
+	18 =
+	19 =
+	20 = 
+
+def blend_easy_bars:
+	21 = "Ayo my pen and paper cause a chain reaction"
+
+	22 = "You beef with me, i'mma even the score equally"
+
+	23 = ""
+	24 =
+	25 =
+	26 =
+	27 = 
+	28 =
+	29 =
+	30 =
+	31 =
+	32 =
+	33 =  
+	34 =
+	35 =
+	36 =
+	37 =
+	38 =
+	39 =
+	40 =
+def legendary_bars:
+	41 = "His palms are sweaty, knees weak, arms are heavy. There's vomit on his sweater already. Mom's spaghetti"
+	42 = "Now, once upon a time not long ago, there was a little rapper about to blow. But his album came, and it was not good. I think it went lead or double copper-wood"
