fe56c60bbf86136c9f28098aade43229308e67c6
yeezus24 <32495689+yeezus24@users.noreply.github.com>
2017-10-25 14:48:02 +0100

    tidied up the code and comments

--- gamegit.py
+++ gamegit.py
@@ -12,44 +12,61 @@ from gameparser import *
 from Bars import *
 
 
+""" for finding the current best score from the spreadsheet example.xls """
 workbook = xlrd.open_workbook('example.xls')
-worksheet = workbook.sheet_by_name('A Test Sheet') # these commands find the spreadsheet with the best score
+worksheet = workbook.sheet_by_name('A Test Sheet') 
 
+""" sets up example.xls to recieve a new username and score if the score from this game surpasses current best score"""
 wb = xlwt.Workbook()
-ws = wb.add_sheet('A Test Sheet') # this setup spreadsheet for inputting the score if it is better than the one in the spreadsheet currently
+ws = wb.add_sheet('A Test Sheet')
 
+""" starts the player off with 0 score """
 global player_score
-player_score = 0 # start player with 0 score
+player_score = 0 
 
+""" begins the timer for the game """
 start_time = datetime.datetime.now()
 
+""" this multiplier will change dependant on user input for difficulty in game's menu """
 global difficulty_multiplier
-difficulty_multiplier = 1.0 # this multiplier will change dependant on user input for difficulty in game's menu
+difficulty_multiplier = 1.0 
 
+
+""" this function is for clearing screen between rooms """
 def cls():
-            os.system('cls' if os.name=='nt' else 'clear') # this is the function for clearing screen
+
+            os.system('cls' if os.name=='nt' else 'clear') 
 
 cls()
 
+
+""" this function takes a string and integer for typing speed to make a typing aesthetic """
 def slow_type(statement, typing_speed):
+
     for letter in statement:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(random.random()*10.0/typing_speed) # this function allows use of slow_type which prints strings with a typing effect
+        time.sleep(random.random()*10.0/typing_speed) 
 
+
+""" first menu when the game is executed, which appears once """
 def screen_menu():
-    """First menu when the game is executed. Appears once."""
+
     slow_type("\n" + "\n" + "RAPPING GAME (title in progress)" + "\n" + "\n" + "\n" + "Your goal is to become the best rapper, and beat Eminem at the Rap Concert." + "\n", 400)
     input("")
     slow_type("The less rappers you have to destroy, the better your overall score will be. The lower your score - the better!" + "\n" + "\n", 400)
     input("")
+
     global username
     slow_type("What is your name?:" + "\n", 400)
     username = input("> ")
+
     slow_type("Chose your difficulty - harder difficulty means better score! EASY / MEDIUM / HARD: " + "\n", 400)
     difficulty = input("> ")
     
-    global difficulty_multiplier # now uses the user input to edit difficulty_multiplier, which will change time given in rooms AND difficulty of rap given to user to input
+    """ users the user input for difficulty to edit variable difficulty_multiplier """
+    global difficulty_multiplier
+
     if difficulty.lower() == "easy":
         difficulty_multiplier = 1.5 
     elif difficulty.lower() == "medium":
@@ -58,6 +75,8 @@ def screen_menu():
         difficulty_multiplier = 0.8
     elif difficulty.lower() == "legendary":
         difficulty_multiplier = 0.750
+        slow_type("You've found the hidden legendary mode! While this mode is very difficult, it is still possible, and will guarantee you a great score", 400)
+        input("")
     else:
         slow_type("That's not an option", 400)
         difficulty = input("easy/medium/hard: ")
@@ -65,17 +84,18 @@ def screen_menu():
     cls()
 
 
-
+""" calculate reputation and returns it via players inventory """
 def calculate_reputation(inventory):
-    """Calculate reputation and returns it via players inventory."""
+
     reputation = 0
-    for item in inventory:  # iterates through every item in the players inventory (see player.py)
-        reputation = reputation + item["reputation"]  # Reputation is added to by each item
-    return reputation  # The total rep is returned to the function that called it (probs the battle one.)
+    for item in inventory:    # iterates through every item in the players inventory (see player.py)
+        reputation = reputation + item["reputation"]    # Reputation is added to by each item
+    return reputation    # The total reputation is returned to the function that called it
 
 
+""" return a random bar depending on the difficulty for the battle """
 def produce_bar():
-    """Return a random bar depending on the difficulty for the battle."""
+
     global difficulty_multiplier
     a = float(difficulty_multiplier)
     length = len(difficulty_dict[a])
@@ -83,10 +103,12 @@ def produce_bar():
     return difficulty_dict[a][choice]
 
 
+""" function that completes battle, takes a timed input, gives player item etc """
 def battle():
-    """Function that completes battle, takes a timed input, gives player item etc."""
+
     global difficulty_multiplier
     global player_score
+
     players_rap = produce_bar()
     print("")
     
@@ -95,24 +117,26 @@ def battle():
     slow_type("You must type word for word, letter for letter." + "\n", 400)
     input("")
     yes_or_no = input("Ready? y / n: " + "\n" + "> ")
+
     if yes_or_no == "y":
-        z = datetime.datetime.now()  # gets the exact time that the user begins inputting the rap
+        z = datetime.datetime.now()    # gets the exact time that the user begins inputting the rap
         print("")
         rap_input = normalise_in(input("> "))
 
-        if rap_input == normalise_in(players_rap):  # if the user input is exactly what the room's rap is
-            b = datetime.datetime.now()  # record the time that the user entered their input
-            c = b - z  # work out how long it took the user to input by subtracting the start time from the end time
-            divmod(c.days * 86400 + c.seconds, 60)  # this formats the time into something that can be used in an if statement
+        if rap_input == normalise_in(players_rap):    # if the user input is exactly what the room's rap is
+            b = datetime.datetime.now()    # record the time that the user entered their input
+            c = b - z    # work out how long it took the user to input by subtracting the start time from the end time
+            divmod(c.days * 86400 + c.seconds, 60)    # this formats the time into something that can be used in an if statement
             d = current_room["room_difficulty"] * difficulty_multiplier
             total_time = d + int(calculate_reputation(inventory))
             
-            if c.seconds > total_time:  # if the user time is larger than what the room's allocated time is
+            if c.seconds > total_time:    # if the user time (c.seconds) is larger than what the room's allocated time is
                 slow_type("You didn't rap fast enough!", 400)
                 input("")
                 slow_type("\n" + "That took you " + str(c.seconds) + " seconds, which is " + str(c.seconds - total_time) + " seconds more than it should have." + "\n", 400)
                 input("")
                 slow_type("You don't have to type in punctuation - that's not the rappers way" + "\n", 400)
+
                 global player_score
                 player_score += 10
                 input("[ENTER]")
@@ -122,7 +146,7 @@ def battle():
                 current_room["rapperbeat"] = True
                 input("")
                 slow_type("That took you " + str(c.seconds) + " seconds." + "\n", 400)
-                inventory.append(current_room["award"]) # adds an item to inventory which gives the user more time to rap in the next rooms
+                inventory.append(current_room["award"])   # adds an item to inventory which gives the user more time to rap in the next rooms
                 input("")
                 slow_type("\n" + "Winning the rap battle got you a " + current_room["award"]["name"] + "." + "\n", 400)
                 input("")
@@ -145,16 +169,18 @@ def battle():
         input("")
 
 
+""" prints a list of inventory items """
 def print_inventory_items(players_items):
-    """Prints a list of inventory items."""
+
     if len(players_items) == 0:
         slow_type("You currently have no items", 400)
     else:
         slow_type("You have" + list_of_items(players_items) + ".", 400)
 
 
+""" returns a string with all the players items in a list """
 def list_of_items(items):
-    """Returns a string with all the players items in a list."""
+
     full_list = ""
     i = 0
     for item in items:
@@ -166,8 +192,9 @@ def list_of_items(items):
     return full_list
 
 
+""" prints the description of the current room """
 def print_room(current_room):
-    """Prints the description of the current room."""
+
     print("")
     slow_type(current_room["name"].upper() + "\n", 400)
     print("")
@@ -175,18 +202,21 @@ def print_room(current_room):
     print("")
 
 
+""" returns exits of a room """
 def exit_leads_to(exits, direction):
-    """Returns exits of a room."""
+
     return rooms[exits[direction]]["name"]
 
 
+""" tells the user about an exit """
 def print_exit(direction, leads):
-    """Tells the user about an exit."""
+
     slow_type("\n""GO " + direction.upper() + " to " + leads + ".", 400)
 
 
+""" prints the list of options for the user to choose """
 def print_menu(exits):
-    """Prints the list of options for the user to choose."""
+
     print_inventory_items(inventory)
     print("")
     input("")
@@ -200,8 +230,8 @@ def print_menu(exits):
         print("")
 
 
+""" sends the program to the current execute function """
 def execute_command(command):
-    """Sends the program to the current execute function."""
 
     if 0 == len(command):
         return
@@ -222,76 +252,91 @@ def execute_command(command):
         input("")
 
 
+""" sends the user to a new room """
 def execute_go(direction):
-    """Sends the user to a new room."""
+
     global current_room
     if direction in current_room["exits"]:
-        a = (current_room["exits"])
+        a = (current_room["exits"])    # a is the dictionary of exits for the current room
 
-        b = a[direction]
+        b = a[direction]    # b is the relevant room given using the key of direction
 
-        current_room = rooms[b] # changes room using direction and current room's exits
+        current_room = rooms[b]    # changes the current room to the value of the direction key
 
     else:
         print("You need to pick a valid direction")
 
 
-
+""" produces the menu via print_menu and takes the user's input """
 def menu(exits):
-    """Produces the menu via print_menu and takes the user's input."""
+
     print_menu(current_room["exits"])
     print("")
     user_input = input("\n" + "> ")
 
-    normalised_user_input = normalise_input(user_input, False)
+    normalised_user_input = normalise_input(user_input, False)    # normalises the user's input
 
     
-    if user_input == "q": # this is mainly for ease of developing the game
+    if user_input == "q":    # this is mainly for ease of developing the game
         raise SystemExit()
 
     else:
         return normalised_user_input
 
 
+""" the main program that controls the flow of the program """
 def main():
-    """The main program that controls the flow of the program."""
+
     screen_menu()
     while True:
 
         print_room(current_room)
         input("")
+
         command = menu(current_room["exits"])
-        execute_command(command) # execute either GO or BATTLE
+
+        execute_command(command)    # execute either GO or BATTLE
+
         calculate_reputation(inventory)
-        
+
         cls()
+
         if rooms["Concert"]["rapperbeat"] == True:
-            end_time = datetime.datetime.now() # ends the timer that has ran throughout the game
+            end_time = datetime.datetime.now()    # ends the timer that has ran throughout the game
             total_game_time = end_time - start_time
-            divmod(total_game_time.days * 86400 + total_game_time.seconds, 60) # formats time game has been played into seconds
+            divmod(total_game_time.days * 86400 + total_game_time.seconds, 60)    # formats time game has been played into seconds
             scr = total_game_time.seconds
+
+
             global player_score
             player_score += scr
             player_score = player_score * difficulty_multiplier
 
-            if difficulty_multiplier == 0.750: # if the game has been played on legendary mode
+
+            if difficulty_multiplier == 0.750:    # if the game has been played on legendary mode
                 player_score = player_score / 4
 
-            if difficulty_multiplier == 1.5: # if the game has been played on easy mode
+            if difficulty_multiplier == 1.5:    # if the game has been played on easy mode
                 player_score = player_score + 200
 
-            if difficulty_multiplier == 1.0: # if the game has been played on medium mode
+            if difficulty_multiplier == 1.0:    # if the game has been played on medium mode
                 player_score = player_score + 100
+
 
             slow_type("\n" + "You have beaten one of the greatest rappers ever and performed for thousands," + "\n", 400)
             slow_type("\n" + "Your score for the game was " + str(player_score) + " points!" + "\n", 400)
+
             
-            best_score = worksheet.row(0)[0].value # takes value of score from excel spreadsheet
-            if float(player_score) < best_score: # if the users score is lower (better) than the score on the spreadsheet (current best score)
+            best_score = worksheet.row(0)[0].value    # takes value of score from excel spreadsheet
+            if float(player_score) < best_score:    # if the users score is lower (better) than the score on the spreadsheet (current best score)
                 print("You have the best score of the day - so far!")
-                ws.write(0, 0, player_score) # adds the player's score to the spreadsheet, in place of the previous best score
-                ws.write(1, 0, username) # adds the user's name in place of the current user's name on the spreadsheet
-                wb.save('example.xls') # saves the excel document
+                ws.write(0, 0, player_score)    # adds the player's score to the spreadsheet, in place of the previous best score
+                ws.write(1, 0, username)    # adds the user's name in place of the current user's name on the spreadsheet
+                wb.save('example.xls')    # saves the excel document
+            else:
+                slow_type("The best score of the day so far is " + str(best_score) + ".", 400)
+                slow_type("\n" + "Playing on a harder difficulty will make it easier to get a better score!", 400)
+                slow_type("\n" + "Rumour has it the devs left a 'legendary' mode in the code, it might be worth checking out?", 400)
 
             break
 
--- roomsgit.py
+++ roomsgit.py
@@ -2,75 +2,63 @@ from items import *
 
 room_studio = {
 	"name": "Studio",
-	"rap": "this ting go skraa, pap pap kak kak kak, skiddy skit pap pap, an a pop pop truu boom",
 	"room_difficulty": 14,
 	"description": "The studio is small, and smells of marijuana. The coat rack is empty, man's not hot.",
 	"rapper": "Roadman Shaq",
 	"rapperbeat": False,
 	"exits": {"east": "Warehouse", "south": "Street Corner", "north": "Concert", "east": "Recording Booth"},
-	"items": [],
 	"award": item_hat
 
 }
 
 room_snoop = {
 	"name": "Snoop Dogg Mansion",
-	"rap": "Now, that, I got me some Seagram's gin, everybody got they cups, but they ain't chipped in.",
-	"room_difficulty": 5,
+	"room_difficulty": 13,
 	"description": "This is Snoop Doggs place of relaxation, do not get him angry!!",
 	"rapper": "Snoop Dogg",
 	"exits": {"east": "Main Street", "south":"2Pac & Biggie"},
 	"rapperbeat": False,
-	"items": [],
 	"award": item_ring
 	
 }
 
 room_2pac_biggie = {
 	"name": "2Pac & Biggie",
-	"rap": "Bein' born with less, I must confess only adds on to the stress",
-	"room_difficulty": 7,
+	"room_difficulty": 12,
 	"description": "It's 2Pac & Biggies favorite club",
 	"rapper": "2Pac & Biggie",
 	"exits": {"east": "Grove Street","north":"Snoop Dogg Mansion"},
 	"rapperbeat": False,
-	"items": [],
 	"award": item_car
 	
 }
 room_warehouse = {
 	"name": "Warehouse",
-	"rap": "Kung fu kenny now, my resume is real enough for two millenniums",
 	"room_difficulty": 14,
 	"description": "It's a dimly lit warehouse, the air is thick with smoke, and floors littered with burnt out joints.",
 	"rapper": "Kendrick Lamar",
 	"exits": {"west": "Studio", "north": "Grove Street"},
 	"rapperbeat": False,
-	"items": [],
 	"award": item_dreadlocks
 }
 
 room_streetcorner = {
 	"name": "Street Corner",
-	"rap": "Who dat boy, who him is",
-	"room_difficulty": 3,
+	"room_difficulty": 9,
 	"description": "There is a crowd of gang members",
 	"rapper": "Tyler, the Creator",
 	"exits": {"north": "Studio"},
 	"rapperbeat": False,
-	"items": [],
 	"award": item_mic
 }
 
 room_concert = {
 	"name": "Rap Concert",
-	"rap": "a suma luma duma luma you assumin I'm a human, what I got to do to get it through to you I'm superhuman",
 	"room_difficulty": 2,
 	"description": "Eminem stands proudly on stage, as thousands of fans scream his name. Out of the crowd he spots you, and invites you to challenge him to a rap battle - live!",
 	"rapper": "Eminem",
 	"exits": {"south": "Studio"},
 	"rapperbeat": False,
-	"items": [],
 	"award": item_tattoo
 }
 
@@ -78,8 +66,6 @@ jail_entrance = {
 	"name": "Jail Entrance",
 	"description": "You have just been released from jail.",
 	"exits": {"south":"Main Street"},
-	"items":[],
-	"rap": "",
 	"n":0,
 	"rapperbeat": False,
 	"rapper":"",
@@ -91,8 +77,6 @@ main_street = {
 	"name": "Main Street",
 	"description": "You are on the Main Street.",
 	"exits": {"north":"Jail Entrance", "south":"Grove Street", "west": "Snoop Dogg Mansion"},
-	"items":[],
-	"rap": "I'm bout to make this mic short circuit, this beat is whack and fits you perfect",
 	"room_difficulty":16,
 	"rapperbeat": False,
 	"rapper":"Novice Rapper",
@@ -103,8 +87,6 @@ grove_street = {
 	"name": "Grove Street",
 	"description": "You are in your old cul-de-sac. Your old home.",
 	"exits": {"north":"Main Street", "south": "Warehouse", "west": "2Pac & Biggie"},
-	"items":[],
-	"rap":"",
 	"room_difficulty":14,
 	"rapperbeat": False,
 	"rapper":"Ice Cube",
@@ -116,8 +98,6 @@ room_recordingbooth = {
 	"name": "Recording Booth",
 	"description": "You are in a retro recording booth, and a dimly lit character stands in the corner of the room, joint in hand.",
 	"exits": {"west": "Studio"},
-	"items":[],
-	"rap":"",
 	"room_difficulty": 14,
 	"rapperbeat": False,
 	"rapper": "Snoop Dogg",
